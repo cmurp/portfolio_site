7518999d75d9ff3d58da5c4089f6e5098aa595eb
cmurp <rajonwitness@gmail.com>
2023-10-13 12:42:00 -0400

    added nav buttons to landing page

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -32,6 +32,7 @@ const moveBackground = keyframes`
 `;
 
 const MainText = styled.h1`
+  margin-top: -20%;
   font-size: 3em;
   text-shadow: 0px 1px 0px #fff, 0px 2px 0px #fff, 0px 3px 0px #ccc, 0px 4px 0px #ccc, 0px 5px 0px #aaa, 0px 6px 1px rgba(0,0,0,.1), 0px 0px 5px rgba(0,0,0,.1), 0px 1px 3px rgba(0,0,0,.3), 0px 3px 5px rgba(0,0,0,.2), 0px 5px 10px rgba(0,0,0,.25), 0px 10px 10px rgba(0,0,0,.2), 0px 20px 20px rgba(0,0,0,.15);
   position: relative;
@@ -56,6 +57,22 @@ const TagLine = styled.span`
   }
 `;
 
+const NavButton = styled.button`
+  background-color: transparent;
+  border: 1px solid white;
+  border-radius: 5px;
+  padding: 0.5em 1em;
+  color: white;
+  font-size: 1.5em;
+  margin-top: 1em;
+  cursor: pointer;
+  transition: all 0.5s ease;
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+`;
+
 const Container = styled.div`
   height: 100vh;
   display: flex;
@@ -76,8 +93,11 @@ const Landing: React.FC = () => {
     <Container>
       <MainText>Chris Murphy</MainText>
       <TagLine>
-        Web Developer & Software Engineer
+        Full Stack Software Engineer
       </TagLine>
+      <NavButton>About Me</NavButton>
+      <NavButton>View My Work</NavButton>
+      <NavButton>Contact Me</NavButton>
     </Container>
   );
 };
