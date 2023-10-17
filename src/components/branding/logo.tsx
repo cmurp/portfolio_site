c6d01c035a8c10da18c07c7b9fb18b500ae31a5c
cmurp <rajonwitness@gmail.com>
2023-10-17 12:22:47 -0400

    add generic earth icon logo

--- src/components/branding/logo.tsx
+++ src/components/branding/logo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BsGlobeAmericas } from 'react-icons/bs';
 
 interface LogoProps {
   alt: string;
@@ -10,11 +11,20 @@ const StyledLogo = styled.img`
   max-height: 100%;
 `;
 
+const LogoContainer = styled.div`
+  font-size: 2rem;
+`
+
 //const logonum=Math.floor(Math.random() * 4) + 1;
 const src = 'logo4.png';
 
 const Logo: React.FC<LogoProps> = ({ alt }) => {
-  return <StyledLogo src={src} alt={alt} />;
+  return (
+  <LogoContainer>
+    <BsGlobeAmericas/>
+  </LogoContainer>
+  );
+  // return <StyledLogo src={src} alt={alt} />;
 };
 
 export default Logo;
