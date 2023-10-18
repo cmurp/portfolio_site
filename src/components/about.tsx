6c9632030bb38dc1d2c435c9243789e75fe88d00
cmurp <rajonwitness@gmail.com>
2023-10-18 13:36:37 -0400

    added 3d globe to about page

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import World from "./world.jsx";
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
-    height: 100vh;
+    width: 80%;
     align-items: center;
 `;
 
@@ -34,6 +35,21 @@ const AccentedDescription = styled.h1`
     -webkit-text-stroke: .5px ${(props: any) => props.theme.colors.accent};
 `;
 
+const StyledLink = styled.a`
+    color: ${(props: any) => props.theme.colors.accent};
+    text-decoration: underline;
+    &:hover {
+        color: ${(props: any) => props.theme.colors.textPrimary};
+        background-color: ${(props: any) => props.theme.colors.accent};
+    }
+`;
+
+const BoldSpan = styled.span`
+    font-weight: bold;
+    color: #0000;
+    -webkit-text-stroke: .5px ${(props: any) => props.theme.colors.accent};
+`;
+
 
 const About = () => {
     return (
@@ -43,6 +59,9 @@ const About = () => {
                 <BoldName>I'm Chris Murphy,</BoldName>
                 <AccentedDescription>full stack software engineer.</AccentedDescription>
             </HelloWorld>
+            <World />
+            <p>Organizing toys in <BoldSpan>Summerfield, NC</BoldSpan></p>
+            <p>Organizing bytes at <StyledLink href="https://www.ibm.com/products/z-anomaly-analytics">IBM Z Anomaly Analytics</StyledLink></p>
         </Container>
     )};
 
---
+++ src/components/world.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, useRef } from "react";
+import Globe from 'react-globe.gl';
+
+const Summerfield = {
+    latitude: 36.2087,
+    longitude: -79.9048,
+    name: "Summerfield, NC",
+    population: 11000
+}
+
+const World = (props) => {
+    const [places, setPlaces] = useState([]);
+    const globeEl = useRef();
+
+    useEffect(() => {
+      setPlaces([Summerfield]);
+      // globeEl.current.controls().autoRotate = true;
+      globeEl.current.pointOfView({ lat: 25, lng: -90, altitude: 2 });
+    }, []);
+
+    return (<Globe
+      ref={globeEl}
+      globeImageUrl="world.jpg"
+      height={400}
+      width={400}
+      backgroundColor="rgba(0,0,0,0)"
+      labelsData={places}
+      labelLat={d => d.latitude}
+      labelLng={d => d.longitude}
+      labelText={d => d.name}
+      labelSize={5}
+      labelDotRadius={1}
+      labelColor={() => 'rgba(255, 165, 0, 0.75)'}
+
+      pointsData={places}
+      pointLat={d => d.latitude}
+      pointLng={d => d.longitude}
+      pointLabel={d => d.name}
+      pointRadius={.5}
+      pointAltitude={.5}
+      pointColor={() => 'rgba(255, 165, 0, 0.75)'}
+    />);
+};
+
+export default World;
