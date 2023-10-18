c10e13d440166053a114c8e6d89acdccda17441e
cmurp <rajonwitness@gmail.com>
2023-10-17 21:05:44 -0400

    add about me intro

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,21 +1,49 @@
 import styled from "styled-components";
 
-const StyledAbout = styled.div`
+const Container = styled.div`
     display: flex;
     flex-direction: column;
+    height: 100vh;
+    align-items: center;
+`;
+
+const HelloWorld = styled.div`
     flex: 1;
     padding: 2rem;
-    overflow-y: scroll;
-    background-color: ${(props: any) => props.theme.colors.main};
+    /* background-color: #00000099; */
     color: ${(props: any) => props.theme.colors.textPrimary};
+    font-size: 2rem;
+    `;
+
+const UnderstatedHello = styled.h1`
+    font-size: 2rem;
+    color: ${(props: any) => props.theme.colors.accent};
+    font-weight: normal;
 `;
+
+const BoldName = styled.h1`
+    font-size: 2rem;
+    color: ${(props: any) => props.theme.colors.textPrimary};
+
+`;
+
+const AccentedDescription = styled.h1`
+    font-size: 2rem;
+    font-weight: bold;
+    color: #0000;
+    -webkit-text-stroke: .5px ${(props: any) => props.theme.colors.accent};
+`;
+
 
 const About = () => {
     return (
-        <StyledAbout>
-            <h1>About</h1>
-            <p>Some information about me.</p>
-        </StyledAbout>
+        <Container>
+            <HelloWorld>
+                <UnderstatedHello>Hi!</UnderstatedHello>
+                <BoldName>I'm Chris Murphy,</BoldName>
+                <AccentedDescription>full stack software engineer.</AccentedDescription>
+            </HelloWorld>
+        </Container>
     )};
 
 export default About;
